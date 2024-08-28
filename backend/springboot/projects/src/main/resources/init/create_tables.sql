8b9edd57087cb793a4495e95b859fc5ad0b86400
minju0809 <loverminju88@naver.com>
2024-08-28 21:29:38 +0900

    [feat] 배송 정보 테이블 추가

--- backend/springboot/projects/src/main/resources/init/create_tables.sql
+++ backend/springboot/projects/src/main/resources/init/create_tables.sql
@@ -23,20 +23,24 @@ CREATE TABLE IF NOT EXISTS carts (
     FOREIGN KEY (product_id) REFERENCES products(product_id) -- 상품 외래키
 );
 
--- 유저 추가 정보 테이블
-CREATE TABLE IF NOT EXISTS user_infos (
-    id INT UNIQUE,                                   -- 사용자 ID (외래키)
+-- 배송 정보 테이블
+CREATE TABLE IF NOT EXISTS shipping_infos (
+    shipping_id INT AUTO_INCREMENT PRIMARY KEY,      -- 배송정보 ID (기본키)
+    id INT NOT NULL,                                 -- 사용자 ID
     name VARCHAR(255) NOT NULL,                      -- 고객명
-    phone VARCHAR(20),                              -- 전화번호
-    address TEXT,                                   -- 주소
+    phone VARCHAR(20),                               -- 전화번호
+    address TEXT,                                    -- 주소
+    is_default tinyint(1),                           -- 기본 배송지 1-true / 0-false
+    nickname VARCHAR(50),                            -- 배송지명
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,  -- 생성 일시
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, -- 수정 일시
     FOREIGN KEY (id) REFERENCES users(id) -- 사용자 외래키
 );
 
+
 -- 주문 정보 테이블
 CREATE TABLE IF NOT EXISTS orders (
-    order_id INT AUTO_INCREMENT PRIMARY KEY,           -- 주문 ID (기본키)
+    order_id INT NOT NULL,                             -- 주문 ID
     id INT NOT NULL,                                   -- 사용자 ID (외래키)
     product_id INT NOT NULL,                          -- 상품 ID (외래키)
     quantity INT NOT NULL,                             -- 수량
